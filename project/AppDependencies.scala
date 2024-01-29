8f90559b33b31346fbc902db16d65bb2762eef7d
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2024-01-29 09:53:22 +0000

    APIS-6724 - Play 3.0 upgrade (#27)
    * APIS-6724 - Play 3.0 upgrade
    
    * Pr-bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,15 +5,14 @@ import sbt._
 
 object AppDependencies {
 
-  lazy val bootstrapVersion = "7.12.0"
+  lazy val bootstrapVersion = "8.4.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion,
-    "org.scalatest"           %% "scalatest"                  % "3.2.17",
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.62.2"
-  ).map(_ % "test, it")
+    "uk.gov.hmrc"         %% "bootstrap-test-play-30" % bootstrapVersion,
+    "com.vladsch.flexmark" % "flexmark-all"           % "0.62.2"
+  ).map(_ % "test")
 }
